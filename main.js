287a4dd4852ff1345eb0f15c636bd2e93f1e12a5
Anthony-S58 <tonysim01@hotmail.com>
2021-06-15 11:09:34 +0200

    défi terminé

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onscroll = function() { scrollIndicator() } ;
+function scrollIndicator() {
+ // Votre code ici
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+
+console.log (scrolled);
+console.log(winScroll);
+console.log(height);
+document.getElementById("bar").style.width = scrolled + "%";
+
+
+
+
+
+
+
+
+}
